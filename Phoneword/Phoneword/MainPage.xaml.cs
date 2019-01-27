c47d0312150e9164cf7c87cfff757a88568f3473
Steve Bilogan <steve.bilogan@gmail.com>
2019-01-27 14:34:30 -0800

    Add DialerService and update main UI to translate and make calls

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -9,9 +9,38 @@ namespace Phoneword
 {
     public partial class MainPage : ContentPage
     {
+        private string _translatedNumber;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            _translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(_translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + _translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + _translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                dialer?.Dial(_translatedNumber);
+            }
         }
     }
 }
